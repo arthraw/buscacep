179f06c17892a063d74bbfb823aea4122138df22
arthraw <arthursallesandrade@gmail.com>
2023-12-28 18:12:22 -0300

    build: Junit and espresso dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,12 +39,19 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+
+    // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0") // para converter JSON em objetos Kotlin
-
-    implementation("com.google.android.material:material:1.9.0")
+    // UI libs
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Tests libs
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation ("androidx.test:rules:1.5.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
